2fb7b248abfcc7e7eaa3fe9c52bce82ba46bb76d
Erich Stehr <Erich.Stehr@users.noreply.github.com>
2016-04-13 12:59:57 -0700

    Update-StringContents: in-file replacement, both exact and RegEx

---
+++ Update-StringContents.ps1
@@ -0,0 +1,82 @@
+param (
+	[Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+	[string] 
+	# Path to file to replace within
+	$path=$(throw "Requires file to replace within"),
+	[System.Collections.Hashtable]
+	# key,value = exact match, replacement
+	$exact=@{},
+	[System.Collections.Hashtable]
+	# key,value = regular expression to match, replacement
+	$regex=@{},
+	[switch]
+	$passThru=$false,
+	[switch]
+	$WhatIf=$WhatIfPreference,
+	[switch]
+	$Confirm=$false,
+	[switch]
+	$force=$false
+	)
+Begin {
+	$PIPELINEINPUT = (-not $PSBOUNDPARAMETERS.ContainsKey("siteUrl")) #https://social.technet.microsoft.com/Forums/scriptcenter/en-US/f07fd26a-ec59-44a4-8143-dea182ffae70/powershell-mandatory-parameters-lose-pipeline-input?forum=ITCG
+
+	function DoIt($thisOne) {
+		$operating = $true
+		try {
+			$res = @(Get-Content -Path $thisOne | %{
+				$line = $_
+				$exact.GetEnumerator() | %{ $line = $line -replace $_.Key,$_.Value}
+				$regex.GetEnumerator() | %{ $line = ([Regex]$_.Key).Replace($line,$_.Value)}
+				$line
+			})
+		} catch {
+            $operating = $false
+			throw;
+		}
+		if ($operating) {
+			$res | % { Write-Debug $_ }
+		    Set-Content -Path $thisOne -Value $res -Force:$force -Confirm:$Confirm -PassThru:$passThru -WhatIf:$WhatIf
+		}
+	}
+
+}
+Process {
+    if ($PIPELINEINPUT -and ($_ -ne $null)) {
+        $path = $_
+    }
+    $path | %{
+        DoIt $_
+    }
+}
+End {
+}
+
+<#
+.SYNOPSIS
+	Replaces text content in source files
+.DESCRIPTION
+	For each line in each source file, replace the keys of $exact with the
+	appropriate value using the -replace operator, then replace the keys of 
+	$regexp with that appropriate value using [Regex]::Replace. Write the 
+	file back out over itself unless -whatif specified
+.INPUTS
+	FileInfo
+.OUTPUTS
+	string[] status messages
+.EXAMPLE
+	PS> "1","2","1 1" | out-file testfile.txt
+	PS> Update-StringContents -path testfile.txt -exact @{'1'='foo';'2'='bar'}
+	PS> gc testfile.txt
+	foo
+	bar
+	foo foo
+.EXAMPLE
+	PS> "bar","bear","bar bell" | out-file testfile.txt
+	PS> Update-StringContents -path testfile.txt -regex @{'b.*?r'='1';'b.*?l'='2'}
+	PS> gc testfile.txt
+	1
+	1
+	1 2l
+	PS> # last line could be just '2l' due to non-ordering of HashTables
+#>
