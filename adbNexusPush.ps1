8e071736c171b49b0bf9a4ea7a5cdabf6e11791e
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2016-05-23 11:20:59 -0700

    adbNexusPush takes current _PowerShell_ directory

--- adbNexusPush.ps1
+++ adbNexusPush.ps1
@@ -1,4 +1,8 @@
 param ([Parameter(Mandatory=$true,ValueFromPipeline=$true)]$source, $target='/sdcard/Books/Webscriptions')
+begin {
+	$script:ced = [Environment]::CurrentDirectory
+	[Environment]::CurrentDirectory = $PWD
+}
 process {
 	$f = $source 
 	if ($source -isnot [IO.FileSystemInfo]) {
@@ -6,4 +10,7 @@ process {
 	}
 	$f.FullName; 
 	adb -d push "`"$($f.FullName)`"" "`"$target`"" # "`"`"" to cover spaces
+}
+end {
+	[Environment]::CurrentDirectory = $script:ced
 }
