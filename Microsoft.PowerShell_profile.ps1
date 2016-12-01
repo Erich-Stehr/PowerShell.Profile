72dc063fff967a55a2a77225aae391793c72104e
Erich Stehr (Aditi Technologies Private LTD) <Erich-Stehr@users.noreply.github.com>
2016-12-01 11:12:01 -0800

    Needed explicit casting to interfaces to make CollapseToDefinitions work

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,9 @@ function mergetxt {
 # Load posh-git example profile
 . "$(split-path $PROFILE.CurrentUserCurrentHost)\Modules\posh-git\profile.example.ps1"
 
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
--- NuGet_profile.ps1
+++ NuGet_profile.ps1
@@ -1,91 +1,98 @@
 function Import-PSModuleConditionally ($name, [Switch]$quiet)
 {
-	if ((gcm get-module -ea silentlycontinue) -and (Get-Module -ListAvailable $name)) { 
-		import-module $name
-		if (!$quiet) { "$name module imported" }
-	}
+    if ((gcm get-module -ea silentlycontinue) -and (Get-Module -ListAvailable $name)) { 
+        import-module $name
+        if (!$quiet) { "$name module imported" }
+    }
 }
 #Import-PSModuleConditionally PSCX
 Import-PSModuleConditionally StudioShell
 
 function global:ScrubBlankLines
 {
-	$selection = $DTE.ActiveDocument.Selection
-	$s = $selection.TopPoint.CreateEditPoint()
-	$f = $selection.BottomPoint.CreateEditPoint()
+    $selection = $DTE.ActiveDocument.Selection
+    $s = $selection.TopPoint.CreateEditPoint()
+    $f = $selection.BottomPoint.CreateEditPoint()
 
-	if ($s.EqualTo($f)) {
-		$s.StartOfDocument()
-		$f.EndOfDocument()
-	}
-	$selection.MoveToPoint($s)
+    if ($s.EqualTo($f)) {
+        $s.StartOfDocument()
+        $f.EndOfDocument()
+    }
+    $selection.MoveToPoint($s)
 
-	$DTE.Find.FindWhat = '^\s*$'
-	$DTE.Find.Target = [EnvDTE.vsFindTarget]::vsFindTargetCurrentDocument
-	#$DTE.Find.MatchCase = $False
-	#$DTE.Find.MatchWholeWord = $true
-	$DTE.Find.Backwards = $false
-	$DTE.Find.MatchInHiddenText = $true
-	$DTE.Find.PatternSyntax = [EnvDTE.vsFindPatternSyntax]::vsFindPatternSyntaxRegExpr
-	$DTE.Find.Action = [EnvDTE.vsFindAction]::vsFindActionFind
-	do {
-		$fd = $DTE.Find.Execute()
-		if (($fd -eq [EnvDTE.vsFindResult]::vsFindResultError) -or ($fd -eq [EnvDTE.vsFindResult]::vsFindResultNotFound)) {
-			break;
-		}
-		#$DTE.ExecuteCommand('Edit.FindNext')
-		if ($selection.BottomPoint.LessThan($s)) { break; }
-		if (!$selection.TopPoint.LessThan($f)) { break; }
-		$selection.Delete()
-	} while ($true)
-	$selection.MoveToPoint($s, $false)
-	$selection.MoveToPoint($f, $true)	
+    $DTE.Find.FindWhat = '^\s*$'
+    $DTE.Find.Target = [EnvDTE.vsFindTarget]::vsFindTargetCurrentDocument
+    #$DTE.Find.MatchCase = $False
+    #$DTE.Find.MatchWholeWord = $true
+    $DTE.Find.Backwards = $false
+    $DTE.Find.MatchInHiddenText = $true
+    $DTE.Find.PatternSyntax = [EnvDTE.vsFindPatternSyntax]::vsFindPatternSyntaxRegExpr
+    $DTE.Find.Action = [EnvDTE.vsFindAction]::vsFindActionFind
+    do {
+        $fd = $DTE.Find.Execute()
+        if (($fd -eq [EnvDTE.vsFindResult]::vsFindResultError) -or ($fd -eq [EnvDTE.vsFindResult]::vsFindResultNotFound)) {
+            break;
+        }
+        #$DTE.ExecuteCommand('Edit.FindNext')
+        if ($selection.BottomPoint.LessThan($s)) { break; }
+        if (!$selection.TopPoint.LessThan($f)) { break; }
+        $selection.Delete()
+    } while ($true)
+    $selection.MoveToPoint($s, $false)
+    $selection.MoveToPoint($f, $true)
 }
 
 function global:CollapseToDefinitions
 {
-	function DefinitionCollapsar([EnvDTE.CodeElement]$ceRoot)
-	{
-	    if ($ceRoot.Kind -eq [EnvDTE.vscmElement]::vsCMElementNamespace) {
-                (Get-Interface $ceRoot [EnvDTE.CodeNamespace]).Members | % {
-		    DefinitionCollapsar($_)
-		}
-            } ElseIf ($ceRoot.Kind -eq [EnvDTE.vscmElement]::vsCMElementClass) {
-                (Get-Interface $ceRoot [EnvDTE.CodeClass]).Members | % {
-                    DefinitionCollapsar($_)
-                }
-            } ElseIf ($ceRoot.Kind -eq [EnvDTE.vscmElement]::vsCMElementStruct) {
-                (Get-Interface $ceRoot [EnvDTE.CodeStruct]).Members | % {
-                    DefinitionCollapsar($_)
-                }
-            } ElseIf ($ceRoot.Kind -eq [EnvDTE.vscmElement]::vsCMElementFunction) {
-                $DTE.ActiveDocument.Selection.MoveToPoint($ceRoot.GetStartPoint([EnvDTE.vsCMPart]::vsCMPartNavigate))
-                $DTE.ExecuteCommand("Edit.ToggleOutliningExpansion")
-	    } ElseIf ($ceRoot.Kind -eq [EnvDTE.vscmElement]::vsCMElementProperty) {
-                $cp = $null
-                Try {
-                    $cp = (get-interface $ceRoot [EnvDTE.CodeProperty])
-                } Catch [System.Exception] {
-                }
-                If ($cp.Getter -ne $null) {
-                    DefinitionCollapsar($cp.Getter)
-                }
-                If ($cp.Setter -ne $null) {
-                    DefinitionCollapsar($cp.Setter)
-                }
-                $DTE.ActiveDocument.Selection.MoveToPoint($ceRoot.GetStartPoint([EnvDTE.vsCMPart]::vsCMPartWholeWithAttributes))
-                $DTE.ExecuteCommand("Edit.ToggleOutliningExpansion")
+    function DefinitionCollapsar([EnvDTE.CodeElement]$ceRoot)
+    {
+        if ($ceRoot -eq $null) {
+            Write-Debug "DefinitionCollapsar: null"
+            return;
+        }
+        Write-Debug ("DefinitionCollapsar: Kind={0}" -f $ceRoot.Kind)
+        if ($ceRoot.Kind -eq [EnvDTE.vscmElement]::vsCMElementNamespace) {
+            (Get-Interface $ceRoot ([EnvDTE.CodeNamespace])).Members | % {
+                DefinitionCollapsar($_)
             }
+        } ElseIf ($ceRoot.Kind -eq [EnvDTE.vscmElement]::vsCMElementClass) {
+            (Get-Interface $ceRoot ([EnvDTE.CodeClass])).Members | % {
+                DefinitionCollapsar($_)
+            }
+        } ElseIf ($ceRoot.Kind -eq [EnvDTE.vscmElement]::vsCMElementStruct) {
+            (Get-Interface $ceRoot ([EnvDTE.CodeStruct])).Members | % {
+                DefinitionCollapsar($_)
+            }
+        } ElseIf ($ceRoot.Kind -eq [EnvDTE.vscmElement]::vsCMElementFunction) {
+            $DTE.ActiveDocument.Selection.MoveToPoint($ceRoot.GetStartPoint([EnvDTE.vsCMPart]::vsCMPartNavigate))
+            $DTE.ExecuteCommand("Edit.ToggleOutliningExpansion")
+        } ElseIf ($ceRoot.Kind -eq [EnvDTE.vscmElement]::vsCMElementProperty) {
+            $cp = $null
+            Try {
+                $cp = (get-interface $ceRoot ([EnvDTE.CodeProperty]))
+            } Catch [System.Exception] {
+                # eat the exceptions and skip
+            }
+            If ($cp.Getter -ne $null) {
+                DefinitionCollapsar($cp.Getter)
+            }
+            If ($cp.Setter -ne $null) {
+                DefinitionCollapsar($cp.Setter)
+            }
+            $DTE.ActiveDocument.Selection.MoveToPoint($ceRoot.GetStartPoint([EnvDTE.vsCMPart]::vsCMPartWholeWithAttributes))
+            $DTE.ExecuteCommand("Edit.ToggleOutliningExpansion")
         }
-	
-        $cp = $DTE.ActiveDocument.Selection.ActivePoint.CreateEditPoint()
-	
-        $DTE.ExecuteCommand("Edit.StopOutlining")
-        $DTE.ExecuteCommand("Edit.StartAutomaticOutlining")
-        $DTE.ActiveDocument.ProjectItem.FileCodeModel.CodeElements | % {
-            DefinitionCollapsar($_)
-        }
-	
-        $DTE.ActiveDocument.Selection.MoveToPoint($cp)
-        $cp.TryToShow([EnvDTE.vsPaneShowHow]::vsPaneShowCentered)
+    }
+
+    $DTE.ActiveDocument.Activate()
+    $cp = $DTE.ActiveDocument.Selection.ActivePoint.CreateEditPoint()
+
+    $DTE.ExecuteCommand("Edit.StopOutlining")
+    $DTE.ExecuteCommand("Edit.StartAutomaticOutlining")
+    (Get-Interface $DTE.ActiveDocument.ProjectItem.FileCodeModel ([EnvDTE.FileCodeModel])).CodeElements | % {
+        DefinitionCollapsar($_)
+    }
+
+    $DTE.ActiveDocument.Selection.MoveToPoint($cp)
+    $cp.TryToShow([EnvDTE.vsPaneShowHow]::vsPaneShowCentered)
 }
