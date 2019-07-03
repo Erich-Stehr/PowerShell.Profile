329465e280bbf530c05ff91e845306e4028c6696
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2019-07-02 17:20:11 -0700

    Basic Show-MessageBox

---
+++ Show-MessageBox.ps1
@@ -0,0 +1,9 @@
+# from <https://michlstechblog.info/blog/powershell-show-a-messagebox/>
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+param ([String]$Message,
+	[string]$Caption = $null,
+	[System.Windows.Forms.MessageBoxButtons]$MessageBoxButtons = [System.Windows.Forms.MessageBoxButtons]::OK, 
+	[System.Windows.Forms.MessageBoxIcon]$MessageBoxIcon = [System.Windows.Forms.MessageBoxIcon]::None,
+	[System.Windows.Forms.MessageBoxDefaultButton]$MessageBoxDefaultButton = ([System.Windows.Forms.MessageBoxDefaultButton]::Button1),
+	[System.Windows.Forms.MessageBoxOptions]$MessageBoxOptions = 0) #helpFilePath, navigator/keyword, 
+[System.Windows.Forms.MessageBox]::Show($Message, $Caption, $MessageBoxButtons, $MessageBoxIcon, $MessageBoxDefaultButton, $MessageBoxOptions, $false)
