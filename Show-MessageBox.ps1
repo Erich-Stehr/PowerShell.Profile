87d67b9d1d152de3e1ed2a5a41ba6299d43c2e56
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2019-07-03 09:49:59 -0700

    script installs function, doesn't run it

--- Show-MessageBox.ps1
+++ Show-MessageBox.ps1
@@ -1,9 +1,12 @@
-# from <https://michlstechblog.info/blog/powershell-show-a-messagebox/>
+# partially from <https://michlstechblog.info/blog/powershell-show-a-messagebox/>
 [void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-param ([String]$Message,
+
+function global:Show-MessageBox([String]$Message,
 	[string]$Caption = $null,
-	[System.Windows.Forms.MessageBoxButtons]$MessageBoxButtons = [System.Windows.Forms.MessageBoxButtons]::OK, 
-	[System.Windows.Forms.MessageBoxIcon]$MessageBoxIcon = [System.Windows.Forms.MessageBoxIcon]::None,
-	[System.Windows.Forms.MessageBoxDefaultButton]$MessageBoxDefaultButton = ([System.Windows.Forms.MessageBoxDefaultButton]::Button1),
+	[System.Windows.Forms.MessageBoxButtons]$MessageBoxButtons = $([System.Windows.Forms.MessageBoxButtons]::OK), 
+	[System.Windows.Forms.MessageBoxIcon]$MessageBoxIcon = $([System.Windows.Forms.MessageBoxIcon]::None),
+	[System.Windows.Forms.MessageBoxDefaultButton]$MessageBoxDefaultButton = $([System.Windows.Forms.MessageBoxDefaultButton]::Button1),
 	[System.Windows.Forms.MessageBoxOptions]$MessageBoxOptions = 0) #helpFilePath, navigator/keyword, 
-[System.Windows.Forms.MessageBox]::Show($Message, $Caption, $MessageBoxButtons, $MessageBoxIcon, $MessageBoxDefaultButton, $MessageBoxOptions, $false)
+{
+    [System.Windows.Forms.MessageBox]::Show($Message, $Caption, $MessageBoxButtons, $MessageBoxIcon, $MessageBoxDefaultButton, $MessageBoxOptions)
+}
