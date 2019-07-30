7b8e16565776dbe2178b5f53f7f7440448d9ecf0
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2019-07-30 15:15:20 -0700

    Set-WifiSecondary

---
+++ Set-WifiSecondaryDNSToOpenDNS.ps1
@@ -0,0 +1,10 @@
+# older method: netsh interface ip set dns name="Ethernet0" static 1.1.1.1 index=2
+$wifi = Get-DnsClientServerAddress "Wireless Network Connection" | ? { $_.Address }
+if (!$wifi) { throw "No 'Wireless Network Connection'" }
+if ($wifi.Address[1] -and $wifi.Address[1] -ne '208.67.222.222') {
+	$addresses = $wifi.Address.Clone()
+	$addresses[1] = '208.67.222.222'
+	Set-DnsClientServerAddress -InterfaceIndex ($wifi.InterfaceIndex) -ServerAddresses $addresses #-Validate
+} else {
+"OK"
+}
