82f48404908fc6f67606f1a9f47615960461c78b
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2019-08-02 15:34:24 -0700

    refresh Git subdirectories

---
+++ GitRefresh.ps1
@@ -0,0 +1,1 @@
+﻿dir | ? { $_.IsPsContainer } | % { ""; pushd -pass $_ | select -expandproperty Path ; git branch --show-current ; git pull --all -p ; GitRemoveTrackingBranchesNoLongerRemote.ps1 ; popd }
