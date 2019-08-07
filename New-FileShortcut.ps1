e803b7e9d8c0f09d34dc5d41fd0d32e2334b7b72
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2019-08-07 16:20:15 -0700

    Regenerate .URL files from links oneliner

--- New-FileShortcut.ps1
+++ New-FileShortcut.ps1
@@ -1,7 +1,7 @@
-# https://stackoverflow.com/questions/9701840/how-to-create-a-shortcut-using-powershell
+# https://stackoverflow.com/questions/9701840/how-to-create-a-shortcut-using-powershell # modified to allow URLs in SourceExe
 param ( [string]$SourceExe, [string]$ArgumentsToSourceExe, [string]$DestinationPath )
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut($DestinationPath)
 $Shortcut.TargetPath = $SourceExe
-$Shortcut.Arguments = $ArgumentsToSourceExe
+if ($ArgumentsToSourceExe -ne $null) { $Shortcut.Arguments = $ArgumentsToSourceExe }
 $Shortcut.Save()
