78a85511f677eaffeb85edae98f7a43e02466969
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2020-03-07 23:38:21 -0800

    Add -clef, UriBuilder to Seq.psm1::Send-SeqEvent

--- Seq.psm1
+++ Seq.psm1
@@ -11,7 +11,8 @@ function Send-SeqEvent (
     [string] $level,
     $properties = @{},
     [switch] $template,
-    [System.Exception] $exception = $null)
+    [System.Exception] $exception = $null,
+    [switch] $clef)
 {
   if (-not $level) {
     $level = 'Information'
@@ -37,14 +38,37 @@ function Send-SeqEvent (
     $ex = ($exception.ToString() | ConvertTo-Json)
   }
   
-  $body = "{""Events"": [ {
+  if (!$clef) {
+  $global:body = "{""Events"": [ {
     ""Timestamp"": ""$([System.DateTimeOffset]::Now.ToString('o'))"",
     ""Level"": ""$level"",
     ""Exception"": $ex,
     ""MessageTemplate"": $($messageTemplate | ConvertTo-Json),
     ""Properties"": $($allProperties | ConvertTo-Json) }]}"
+  } else {
+    $clefObj = [ordered]@{}
+    $clefObj["@t"] = [System.DateTimeOffset]::Now.ToString('o')
+    $clefObj["@l"] = $level
+    $clefObj["@x"] = $ex
+    $clefObj["@mt"] = $messageTemplate | ConvertTo-Json
+    $clefObj += $allProperties
+    $global:body = ConvertTo-Json -InputObject $clefObj -Compress 
+  }
+
+  Write-Verbose -Verbose $body
+
+  $global:uribuilder = New-Object UriBuilder @($seq["Url"])
+  $uribuilder.Path = "api/events/raw"
+  $queryComponents = @()
+  if ($clef) {
+    $queryComponents += "clef"
+  }
+  if ($seq["ApiKey"]) {
+    $queryComponents += "apiKey=$($seq["ApiKey"])"
+  }
+  $uribuilder.Query = [string]::Join("&", $queryComponents)
+
+  Write-Verbose -Verbose $uribuilder.ToString()
   
-  $target = "$($seq["Url"])/api/events/raw?apiKey=$($seq["ApiKey"])"
-  
-  Invoke-RestMethod -Uri $target -Body $body -ContentType "application/json" -Method POST
+  Invoke-RestMethod -Uri $uribuilder.ToString() -Body $body -ContentType "application/json" -Method POST
 }
