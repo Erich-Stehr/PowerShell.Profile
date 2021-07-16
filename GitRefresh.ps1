7f33dcda3bc5e36755295677df17ea35e771b7cb
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2021-07-15 17:26:57 -0700

    GitRefresh: add -PullBranch to pull PR-merged branches instead of fetching

--- GitRefresh.ps1
+++ GitRefresh.ps1
@@ -1,1 +1,6 @@
-﻿dir $PSScriptRoot -exclude nongit | ? { $_.PsIsContainer } | % { ""; pushd -pass $_ | select -expandproperty Path ; git branch --show-current ; git fetch --all -p ; GitRemoveTrackingBranchesNoLongerRemote.ps1 ; popd }
+﻿param (
+	[string[]]
+	# branch names to pull instead of fetch, as they shouldn't be directly worked in ("master", "main", "dev")
+	$PullBranch = @("master", "main", "dev")
+)
+dir $PSScriptRoot -exclude nongit | ? { $_.PsIsContainer } | % { ""; pushd -pass $_ | select -expandproperty Path ; $branchName = git branch --show-current ; $branchName ; if ($PullBranch.Contains($branchName)) { git pull --all -p } else { git fetch --all -p } ; GitRemoveTrackingBranchesNoLongerRemote.ps1 ; popd }
