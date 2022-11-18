46299528e28a0ac33901d6d46864ee6876cd5464
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2022-11-17 18:47:30 -0800

    helps to close the block....

--- profile.ps1
+++ profile.ps1
@@ -941,4 +941,5 @@ if (test-path 'C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\
             runas /u:$user 'C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe'
         }
     }
+}
 #
