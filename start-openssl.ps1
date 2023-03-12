63fc7dc76062fabd23be18b31202d918f796c1d2
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2023-03-11 22:24:34 -0800

    start-openssl.ps1: add attenpts to repoint to correct locations using environment variables

--- start-openssl.ps1
+++ start-openssl.ps1
@@ -1,2 +1,15 @@
-$env:PATH="${env:PATH};${env:ProgramFiles}\OpenSSL-Win64\bin"
+[CmdletBinding()]
+$openSslDir="${env:ProgramFiles}\OpenSSL-Win64"
+if (!(test-path $openSslDir)) {
+    $openSslDir="${env:ProgramFiles}\OpenSSL"
+}
+if (!(test-path $openSslDir)) {
+    throw "Couldn't find OpenSSL directory!"
+}
+
+$env:PATH="${env:PATH};${openSslDir}\bin"
+$env:OPENSSL_CONF="${env:ProgramFiles}\Common Files\SSL\openssl.cnf"
+Write-Verbose "OPENSSL_CONF=${env:OPENSSL_CONF}"
+$env:OPENSSL_ENGINES="${openSslDir}"
+Write-Verbose "OPENSSL_ENGINES=${env:OPENSSL_ENGINES}"
 openssl version -a
