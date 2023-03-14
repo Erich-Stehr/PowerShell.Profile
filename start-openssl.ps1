168a9dc03006d082eb3d1980a3fb1568cd58d8f0
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2023-03-14 16:34:56 -0700

    start-openssl: correct modules for work drop

--- start-openssl.ps1
+++ start-openssl.ps1
@@ -12,4 +12,6 @@ $env:OPENSSL_CONF="${env:ProgramFiles}\Common Files\SSL\openssl.cnf"
 Write-Verbose "OPENSSL_CONF=${env:OPENSSL_CONF}"
 $env:OPENSSL_ENGINES="${openSslDir}"
 Write-Verbose "OPENSSL_ENGINES=${env:OPENSSL_ENGINES}"
+$env:OPENSSL_MODULES="${openSslDir}\bin"
+Write-Verbose "OPENSSL_MODULES=${env:OPENSSL_MODULES}"
 openssl version -a
