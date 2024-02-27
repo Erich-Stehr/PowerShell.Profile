9914b0b54d2efe23fc30c6fe7fc6966c0625ca92
Erich Stehr <Erich-Stehr@users.noreply.github.com>
2024-02-26 16:14:49 -0800

    add name to container for launching datalust/seq

--- dockerseq.ps1
+++ dockerseq.ps1
@@ -1,1 +1,1 @@
-start-process -FilePath $PSHOME\powershell.exe -ArgumentList @("docker", "run", "--rm", "-it", "-e", "ACCEPT_EULA=Y", "-p", "127.0.0.1:8080:80", "-p", "5341:5341", "datalust/seq")
+start-process -FilePath $PSHOME\powershell.exe -ArgumentList @("docker", "run", "--rm", "-it", "--name", "seq", "-e", "ACCEPT_EULA=Y", "-p", "127.0.0.1:8080:80", "-p", "5341:5341", "datalust/seq")
